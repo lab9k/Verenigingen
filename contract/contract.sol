c9a713951c91e7d8f781c7e2a157dafe03650d50
Jefwillems <jefwillems100@gmail.com>
2017-09-11 13:53:40 +0200

    added basic functionality to contract

--- contract/contract.sol
+++ contract/contract.sol
@@ -3,8 +3,78 @@ pragma solidity ^0.4.16;
 
 contract Owned {
 
+    mapping(address => bool) admins;
+
+    function Owned() {
+        admins[msg.sender] = true;
+    }
+
+    modifier isAdmin(){
+        require(admins[msg.sender]);
+        _;
+    }
+
+    function addAdmin(address _newAdmin) isAdmin {
+        admins[_newAdmin] = true;
+    }
+
+    function removeAdmin(address _admin) isAdmin {
+        admins[_admin] = false;
+    }
+
 }
 
-contract Verenigingen {
-    
+
+contract VerenigingenContract is Owned {
+
+    enum Status { ACCEPTED , PENDING , DENIED }
+
+    struct Vereniging {
+        string naam;
+        string ondernemingsnummer;
+        string beschrijving;
+        Status status;
+    }
+    uint numVerenigingen = 0;
+    mapping(uint => Vereniging) verenigingen;
+
+    function VerenigingenContract() {
+        
+    }
+
+    function acceptRequest(uint id) isAdmin {
+        if (verenigingen[id].status != Status.PENDING) {
+            revert();
+        }
+        verenigingen[id].status = Status.ACCEPTED;
+    }
+
+    function denyRequest(uint id) isAdmin {
+        if (verenigingen[id].status != Status.PENDING) {
+            revert();
+        }
+        verenigingen[id].status = Status.DENIED;
+    }
+
+    function addVereniging(string _naam, string _ondernemingsnummer, string _beschrijving) {
+        numVerenigingen++;
+        verenigingen[numVerenigingen] = Vereniging({
+            naam:_naam,
+            ondernemingsnummer:_ondernemingsnummer,
+            beschrijving:_beschrijving,
+            status:Status.PENDING
+        });                 
+    }
+
+    function editVereniging(
+        uint id,
+        string _naam,
+        string _ondernemingsnummer,
+        string _beschrijving) isAdmin 
+        {
+        verenigingen[id].naam = _naam;
+        verenigingen[id].ondernemingsnummer = _ondernemingsnummer;
+        verenigingen[id].beschrijving = _beschrijving;
+    }
+
 }
