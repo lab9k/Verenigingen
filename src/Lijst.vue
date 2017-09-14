a70f0b16732f3ab8f2455a3740b616840b9091da
bert <bertcommeine@gmail.com>
2017-09-14 17:55:55 +0200

    close #58

--- src/Lijst.vue
+++ src/Lijst.vue
@@ -9,54 +9,44 @@
                       Alle verenigingen
                     </button>
                 </div>
-                <div class="wrapper">
-                  <div class="lijst_item"
-                      v-bind:class="{ open: isActiveListItem }"
-                      v-on:click="isActiveListItem = !isActiveListItem"
-                      v-for="item in lijst" :key='item.id'>
-                      <div class="name">
-                          <label>Naam: </label>
-                          {{ item.naam }}
-                      </div>
-                      <div class="list-wrapper">
-                        <div class="list_item"
-                        v-bind:class="{ open: isActiveListItem }"
-                        v-on:click="isActiveListItem = !isActiveListItem"
-                        v-for="(item, index) in lijst"
-                        :key="index">
-                        <div class="border-left"></div>
-                        <div class="name">
-                            <strong>{{ item.naam }}</strong>
-                        </div>
-                        <div class="ondernemingsnummer">
-                            Ondernemingsnummer:
-                            {{ item.ondernemingsnummer }}
-                        </div>
-                        <div class="description">
-                            {{ item.beschrijving }}
-                        </div>
-                        <div class="status">
-                            <div>{{ item.status }}</div>
-                            <img v-bind:src="'assets/' + item.status + '.svg'" alt="Accepted" title="Status">
-                        </div>
+                <div class="list-wrapper">
+                  <div class="list_item"
+                      v-bind:class="{ open: (activeListItem == item.id) }"
+                      v-on:click="toggleCollapse(item.id)"
+                      v-for="item in lijst" :key='parseInt(item.id)'>
+                            <div class="border-left"></div>
+                            <div class="name">
+                                <strong>{{ item.naam }}</strong>
+                            </div>
+                            <div class="ondernemingsnummer">
+                                Ondernemingsnummer:
+                                {{ item.ondernemingsnummer }}
+                            </div>
+                            <div class="description">
+                                {{ item.beschrijving }}
+                            </div>
+                            <div class="status">
+                                <div>{{ item.status }}</div>
+                                <img v-bind:src="'assets/' + item.status + '.svg'" alt="Accepted" title="Status">
+                            </div>
+                            <button v-on:click='$root.acceptRequest(item.id)' >
+                              goedkeuren
+                            </button>
+                            <button v-on:click='$root.denyRequest(item.id)' >
+                              afkeuren
+                            </button>
                     </div>
+                  </div>
                 </div>
-            </div>
-                <button v-on:click='$root.acceptRequest(item.id)' >
-                  goedkeuren
-                </button>
-                <button v-on:click='$root.denyRequest(item.id)' >
-                  afkeuren
-                </button>
-        </div>
-        <footer>
-            <p>&copy Copyright - Lab9K</p>
-            <p><a href="">GitHub</a></p>
-            <div class="partners">
-                <a href="https://lab9k.github.io/" target="_blank"><img src="assets/partners/partner-lab9k.svg" alt=""></a>
-                <a href="https://stad.gent" target="_blank"><img src="assets/partners/partner-stadgent.svg" alt=""></a>
-            </div>
-        </footer>
+              </div>
+              <footer>
+                  <p>&copy Copyright - Lab9K</p>
+                  <p><a href="">GitHub</a></p>
+                  <div class="partners">
+                      <a href="https://lab9k.github.io/" target="_blank"><img src="assets/partners/partner-lab9k.svg" alt=""></a>
+                      <a href="https://stad.gent" target="_blank"><img src="assets/partners/partner-stadgent.svg" alt=""></a>
+                  </div>
+              </footer>
     </div>
 </template>
 
@@ -66,7 +56,7 @@
         data() {
             return {
                 title: 'Lijst',
-                isActiveListItem: false,
+                activeListItem: -1,
                 lijst: []
             }
         },
@@ -74,6 +64,13 @@
           this.lijst = Object.values(this.$root.verenigingList)
         },
         methods: {
+          toggleCollapse: function(id){
+            if(this.activeListItem == id){
+              this.activeListItem = -1
+            } else {
+              this.activeListItem = id
+            }
+          },
           toonAlle: function(){
             this.lijst = Object.values(this.$root.verenigingList)
           },
