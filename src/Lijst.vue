d69e2d15fc07d8af30465ce3790baf7b88a00c0b
Ismail Kutlu <ismail.kutlu@student.arteveldehs.be>
2017-09-14 16:44:23 +0200

    Homepage and list page designed

--- src/Lijst.vue
+++ src/Lijst.vue
@@ -1,34 +1,44 @@
 <template>
     <div id="lijst">
-      <h2>{{ title }}</h2>
-      <div class="search-wrapper">
-        <input @input="updateLijst" type="text" v-model="search" placeholder="Search title.."/>
-        <label>Search title:</label>
-      </div>
-      <div class="wrapper">
-        <div class="lijst_item"
-              v-bind:class="{ open: isActiveListItem }"
-              v-on:click="isActiveListItem = !isActiveListItem"
-              v-for="(item, index) in lijst"
-              :key="index">
-                <div class="name">
-                    <label>Naam: </label>
-                    {{ item.naam }}
+        <div class="content">
+            <h2>{{ title }}</h2>
+            <div class="min-height">
+                <div class="search-wrapper">
+                    <input @input="updateLijst" type="text" v-model="search" placeholder="Search title.."/>
                 </div>
-                <div class="ondernemingsnummer">
-                    Ondernemingsnummer:
-                    {{ item.ondernemingsnummer }}
+                <div class="list-wrapper">
+                    <div class="list_item"
+                        v-bind:class="{ open: isActiveListItem }"
+                        v-on:click="isActiveListItem = !isActiveListItem"
+                        v-for="(item, index) in lijst"
+                        :key="index">
+                        <div class="border-left"></div>
+                        <div class="name">
+                            <strong>{{ item.naam }}</strong>
+                        </div>
+                        <div class="ondernemingsnummer">
+                            Ondernemingsnummer:
+                            {{ item.ondernemingsnummer }}
+                        </div>
+                        <div class="description">
+                            {{ item.beschrijving }}
+                        </div>
+                        <div class="status">
+                            <div>{{ item.status }}</div>
+                            <img v-bind:src="'assets/' + item.status + '.svg'" alt="Accepted" title="Status">
+                        </div>
+                    </div>
                 </div>
-                <div class="description">
-                    {{ item.beschrijving }}
-                </div>
-                <div class="status">
-                    Status:
-                    <div v-if="item.status == 1">{{ item.status }}</div>
-                </div>
-          </h1>
+            </div>
         </div>
-      </div>
+        <footer>
+            <p>&copy Copyright - Lab9K</p>
+            <p><a href="">GitHub</a></p>
+            <div class="partners">
+                <a href="https://lab9k.github.io/" target="_blank"><img src="assets/partners/partner-lab9k.svg" alt=""></a>
+                <a href="https://stad.gent" target="_blank"><img src="assets/partners/partner-stadgent.svg" alt=""></a>
+            </div>
+        </footer>
     </div>
 </template>
 
@@ -48,9 +58,9 @@
                 item.classList.toggle('open');
             },
             updateLijst: function() {
-              this.lijst = Object.values(this.$root.verenigingList).filter( (post) =>  {
-                return post.naam.toLowerCase().includes(this.search.toLowerCase())
-              })
+                this.lijst = Object.values(this.$root.verenigingList).filter( (post) =>  {
+                    return post.naam.toLowerCase().includes(this.search.toLowerCase())
+                })
             }
         }
     }
